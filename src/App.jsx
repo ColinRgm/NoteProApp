18423b4023011da0f9a407940ce8d9679ec98960
ColinRgm <colin.regamey@icloud.com>
2025-02-20 15:14:48 +0100

    fix: move the navbar in the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import Sidebar from "./components/sidebar.jsx";
-import Content from "./components/content.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar.jsx";
+import Dashboard from "./pages/dashboard.jsx";
+import FirstYear from "./pages/first_year.jsx";
+import SecondYear from "./pages/second_year.jsx";
+import ThirdYear from "./pages/third_year.jsx";
+import FourthYear from "./pages/fourth_year.jsx";
+import Profile from "./pages/profile.jsx";
 
 function App() {
 
     return (
         <>
-            <div className="flex h-screen">
-                {/* Left side */}
-                <div className="w-[225px]">
-                    <Sidebar />
-                </div>
-
-                {/* Right side */}
-                <div className="flex-1">
-                    <Content />
-                </div>
-            </div>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={ <Dashboard/> }/>
+                    <Route
+                        path="/first_year"
+                        element={ <FirstYear/> }/>
+                    <Route
+                        path="/second_year"
+                        element={ <SecondYear/> }/>
+                    <Route
+                        path="/third_year"
+                        element={ <ThirdYear/> }/>
+                    <Route
+                        path="/fourth_year"
+                        element={ <FourthYear/> }/>
+                    <Route
+                        path="/profile"
+                        element={ <Profile/> }/>/
+                </Routes>
+            </Router>
         </>
     )
 }
