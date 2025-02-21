ea64c97c6384bcf693ceabae82b84d9169205d1b
ColinRgm <colin.regamey@icloud.com>
2025-02-21 08:50:02 +0100

    feat: edit the dashboard

---
+++ src/components/average/average_card.jsx
@@ -0,0 +1,22 @@
+function Average_card({ title }) {
+    return (
+
+        <>
+
+            <div>
+                {/* Titre de la carte */ }
+                <div className="relative pr-5 py-3 w-fit mb-[-27px] ml-[-20px] bg-white">
+                    <h1 className="text-xl font-bold">{ title }</h1>
+                </div>
+
+                {/* Contenu de la carte */ }
+                <div className="border-1 h-40 w-40 flex items-center justify-center">
+                    <h1 className="text-3xl font-bold">6</h1>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default Average_card;
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,8 +1,33 @@
+import Grades_card from "../components/grade/grades_card.jsx";
+import Average_card from "../components/average/average_card.jsx";
+
 function Dashboard() {
 
     return (
-        <h1>Dashboard</h1>
+
+        <>
+            <div className="flex flex-col gap-y-10">
+                <div className="flex flex-row justify-between h-full">
+                    <Average_card title="Moyenne générale"/>
+                    <Average_card title="Base élargie" />
+                    <Average_card title="Culture générale"/>
+                    <Average_card title="Informatique"/>
+                </div>
+
+                <div className="flex flex-col">
+                    <Grades_card/>
+                </div>
+
+                <div className=" flex items-center justify-center">
+                    <button className="bg-[#141e66] text-white px-10 py-2 rounded-md hover:bg-[#8fc62e] hover:text-[#141e66] transition">
+                        Ajouter note
+                    </button>
+                </div>
+            </div>
+        </>
+
     )
 
 }
+
 export default Dashboard;
