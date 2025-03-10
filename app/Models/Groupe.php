e0738df79ab7de6da9de1aad17c16399dc059a8e
ColinRgm <colin.regamey@icloud.com>
2025-03-10 15:40:51 +0100

    feat: create a controller and a model for the branche's groupe

---
+++ app/Models/Groupe.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Groupe extends Model
+{
+    /**
+     * Table associée au model
+     *
+     * @return string
+     *
+     */
+    protected $table = 'groupes';
+
+    /**
+     * Un groupe peut contenir une ou plusieurs Branche
+     *
+     * @return BelongsToMany
+     *
+     */
+    public function groupeBy(): BelongsToMany
+    {
+        return $this->belongsToMany(Groupe::class, 'branches');
+    }
+}
