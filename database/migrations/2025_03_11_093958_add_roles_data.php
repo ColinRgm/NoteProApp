e3efc9bba5a7dbf0d161cb804f6c282cda74552f
ColinRgm <colin.regamey@icloud.com>
2025-03-11 10:48:41 +0100

    feat: create the migration and add the primary datas

---
+++ database/migrations/2025_03_11_093958_add_roles_data.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('roles')->insert([
+            ['name' => 'Apprenti'],
+            ['name' => 'Formateur'],
+            ['name' => 'Coach']
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('roles')->delete();
+    }
+};
