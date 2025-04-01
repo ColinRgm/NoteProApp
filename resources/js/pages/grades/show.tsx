6905929a34c293d7301c8baa97045eb61de9ed16
ColinRgm <90181373+ColinRgm@users.noreply.github.com>
2025-04-01 16:46:31 +0200

    feat: each grades have now its own page

--- resources/js/pages/grades/show.tsx
+++ resources/js/pages/grades/show.tsx
@@ -32,7 +32,7 @@ export default function GradeDetail() {
             <div className="flex flex-1 flex-col items-center justify-center gap-20 rounded-xl p-4">
                 <Card className="w-[70%] p-5">
                     <CardTitle size="xxl" margin="big">
-                        {uniqueGrade.branch?.name} - {uniqueGrade.grade}
+                        {uniqueGrade.branch?.name}
                     </CardTitle>
 
                     <CardContent>
