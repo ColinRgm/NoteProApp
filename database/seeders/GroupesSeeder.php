4d2d72e5cd35e883e6669a13ca7f0aecacdc914f
ColinRgm <90181373+ColinRgm@users.noreply.github.com>
2025-04-10 14:46:16 +0200

    feat: rounding error in the seeder

--- database/seeders/GroupesSeeder.php
+++ database/seeders/GroupesSeeder.php
@@ -13,10 +13,10 @@ class GroupesSeeder extends Seeder
     public function run(): void
     {
         Group::insert([
-            ['name' => 'Culture générale', 'weight' => 20, 'rounding' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Compétence de base élargie', 'weight' => 10, 'rounding' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Compétence en informatique', 'weight' => 30, 'rounding' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Travail pratique', 'weight' => 40, 'rounding' => 1, 'created_at' => now(), 'updated_at' => now()]
+            ['name' => 'Culture générale', 'weight' => 20, 'rounding' => 0.5, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Compétence de base élargie', 'weight' => 10, 'rounding' => 0.5, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Compétence en informatique', 'weight' => 30, 'rounding' => 0.5, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Travail pratique', 'weight' => 40, 'rounding' => 0.5, 'created_at' => now(), 'updated_at' => now()]
         ]);
     }
 }
