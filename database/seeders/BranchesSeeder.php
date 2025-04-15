c4cf5d7a2c4a5ce1a42e47743f906d944216d2fc
ColinRgm <colin.regamey@icloud.com>
2025-04-15 14:55:09 +0200

    fix: edit the migration

--- database/seeders/BranchesSeeder.php
+++ database/seeders/BranchesSeeder.php
@@ -13,12 +13,12 @@ class BranchesSeeder extends Seeder
     public function run(): void
     {
         Branch::insert([
-            ['name' => 'ECG', 'weight' => 100, 'rounding' => 0.5, 'groupe_id' => 1, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Math', 'weight' => 50, 'rounding' => 0.2, 'groupe_id' => 2, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'Anglais', 'weight' => 50, 'rounding' => 0.2, 'groupe_id' => 2, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'EPSIC', 'weight' => 80, 'rounding' => 0.2, 'groupe_id' => 3, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'CIE', 'weight' => 20, 'rounding' => 0.2, 'groupe_id' => 3, 'created_at' => now(), 'updated_at' => now()],
-            ['name' => 'TPI', 'weight' => 100, 'rounding' => 0.5, 'groupe_id' => 4, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'ECG', 'weight' => 100, 'rounding' => 0.5, 'group_id' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Math', 'weight' => 50, 'rounding' => 0.2, 'group_id' => 2, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Anglais', 'weight' => 50, 'rounding' => 0.2, 'group_id' => 2, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'EPSIC', 'weight' => 80, 'rounding' => 0.2, 'group_id' => 3, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'CIE', 'weight' => 20, 'rounding' => 0.2, 'group_id' => 3, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'TPI', 'weight' => 100, 'rounding' => 0.5, 'group_id' => 4, 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
