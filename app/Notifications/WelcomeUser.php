8ae9b6a1998e3ef50f12448306d5e8623b2bbe67
ColinRgm <colin.regamey@icloud.com>
2025-04-16 16:29:50 +0200

    feat: create a file for the email notification

---
+++ app/Notifications/WelcomeUser.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeUser extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('Vous avez été ajouté sur la plateforme NoteProApp de Jobtrek')
+            ->greeting('Bonjour ' . $notifiable->name . ' !')
+            ->line('Veuillez vous connectez afin de modifier votre mot de passe')
+            ->action('Voici le lien vers la plateforme', url('/'))
+            ->line('Belle journée !');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
