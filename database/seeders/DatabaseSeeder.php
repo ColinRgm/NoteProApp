6e7e53b0484522456c130b6ec24ab79bdff2a01e
ColinRgm <90181373+ColinRgm@users.noreply.github.com>
2025-04-16 14:18:18 +0200

    fix: edit the general seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $this->call([
+            RolesSeeder::class,
+            UsersSeeder::class,
+            GroupesSeeder::class,
+            BranchesSeeder::class,
+            ModulesSeeder::class
+        ]);
     }
 }
